e45e5ed8a6d39181dcb07dbf0e4178e0e6f5595c
Tejinder4 <tejinder.panwar@jaarvis.com>
2020-10-13 15:32:00 +0530

    commit message

---
+++ test_one/myapp/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Student
+from .models import Teacher
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = '__all__'
